c4df369d9d87e8f93b1fff2baf19afdf64d380fa
csm2017 <mcgahancory@gmail.com>
2020-08-26 13:57:43 -0500

    initial commit

---
+++ DateCore.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DateCore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DateCore.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
