a4a3f8370f47aed7ae575dc866d0e6e7fb07c3a7
csm2017 <mcgahancory@gmail.com>
2020-09-29 17:28:42 -0500

    added interfaces for users and photos. created user service to retrieve users from API. display users on page. styled member-card component

--- DateCore-SPA/src/app/app.module.ts
+++ DateCore-SPA/src/app/app.module.ts
@@ -11,9 +11,10 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 import { appRoutes } from './routes';
 
 @NgModule({
@@ -24,7 +25,8 @@ import { appRoutes } from './routes';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent
    ],
   imports: [
     BrowserModule,
